df22425ed53a0f987f02a640c5f719883e7020fd
Apurba Roy Anik <54495133+apurba667@users.noreply.github.com>
2020-01-17 12:35:44 +0600

    update

--- Demo/src/demo/Demo.java
+++ Demo/src/demo/Demo.java
@@ -4,59 +4,84 @@ class Record {
     private int age;
     private double cgpa;
     private String name,id;
-    
-    public void setAge(int ag)
-    {
-        age=ag;
-    }
-    public void getAge()
-    {
+    private String department;
+    private String campus;
+
+    public int getAge() {
         return age;
     }
-    public void setCGPA(double cg)
-    {
-        cgpa=cg;
-    }
-    public double getCGPA()
-    {
+
+    public double getCgpa() {
         return cgpa;
     }
-    public void setName(String nme)
-    {
-        name=nme;
-    }
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
-    public void setId(String iD)
-    {
-        id=iD;
-    }
-    public String getId()
-    {
+
+    public String getId() {
         return id;
     }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+    
     
 }
 public class Demo {
 
     public static void main(String[] args) {
-        Record student=new Record();
-        student.setId("191-15-12499");
-        student.setAge(20);
-        student.setCGPA(3.45);
-        student.setName("Apurba Roy");
-        
-        int Age=student.getAge();
-        int CGPA=student.getCGPA();
-        String Name=student.getName();
-        String Id=student.getId();
-        
-        System.out.println("Name:"+Name);
-        System.out.println("Id="+Id);
-        System.out.println("Age:"+Age);
-        System.out.println("CGPA="+CGPA);
+       Record student = new Record();
+       student.setAge(20);
+       student.setCampus("Main");
+       student.setCgpa(3.5);
+       student.setDepartment("CSE");
+       student.setId("191-15-12499");
+       student.setName("Apurba Kumar Roy");
+       
+       int studentAge=student.getAge();
+       String studentCampus=student.getCampus();
+       double studentCgpa=student.getCgpa();
+       String studentDepartment=student.getDepartment();
+       String studentId=student.getId();
+       String studentName=student.getName();
+       
+        System.out.println("Name="+studentName);
+        System.out.println("Id="+studentId);
+        System.out.println("Age="+studentAge);
+        System.out.println("Cgpa="+studentCgpa);
+        System.out.println("Campus="+studentCampus);
+        System.out.println("Department="+studentDepartment);
+       
     }
     
 }
