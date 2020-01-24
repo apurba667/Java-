4381b795ea9785277a1b6314f80b560a28c89202
Apurba Roy Anik <54495133+apurba667@users.noreply.github.com>
2020-01-24 13:52:29 +0600

    Add

---
+++ lab3/src/lab3/Lab3.java
@@ -0,0 +1,16 @@
+
+package lab3;
+
+import java.util.Scanner;
+
+public class Lab3 {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       String name = sc.nextLine();
+       Float CGPA = sc.nextFloat();
+       int year = sc.nextInt();
+        System.out.println("My name is "+name+" CGPA is "+CGPA+" Year "+year);
+    }
+    
+}
