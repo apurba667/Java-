137d1e1288f1ea3de39365a524d92ebcb565f2ba
Apurba Roy Anik <54495133+apurba667@users.noreply.github.com>
2020-02-02 21:17:38 +0600

    add

---
+++ simple calculator/src/simple/calculator/SimpleCalculator.java
@@ -0,0 +1,20 @@
+
+package simple.calculator;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=11;
+        int summation = a+b;
+        int subtraction = a-b;
+        int multiplication = a*b;
+        double division = a/b;
+        
+        System.out.println("summation = "+summation);
+        System.out.println("subtraction = "+subtraction);
+        System.out.println("division = "+division);
+        System.out.println("multiplication ="+multiplication);
+    }
+    
+}
